6a0f1a34358e48ca4685c198154d9739a4550eb9
jasonshock14-blip <jasonshock14@gmail.com>
2026-01-18 20:19:49 +0700

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,16 +1,23 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+console.log("CreatorFlow AI: Booting up...");
+
 const rootElement = document.getElementById('root');
 if (!rootElement) {
+  console.error("CreatorFlow AI: Failed to find root element!");
   throw new Error("Could not find root element to mount to");
 }
 
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+try {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+  console.log("CreatorFlow AI: Mount successful.");
+} catch (err) {
+  console.error("CreatorFlow AI: Critical mount error:", err);
+}
